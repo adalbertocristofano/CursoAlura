8750b96d3b8203a17fe89612d23774c282fdd804
Adalberto Cristofano <adalberto.cristofano@benner.com.br>
2018-01-10 18:45:56 -0200

    Cadastro de contas

---
+++ CaixaEletronico/CaixaEletronico/CadastroDeContas.cs
@@ -0,0 +1,32 @@
+﻿using Caelum.CaixaEletronico.Modelo.Contas;
+using Caelum.CaixaEletronico.Modelo.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class CadastroDeContas : Form
+    {
+        private Form1 aplicacaoPrincipal;
+        public CadastroDeContas(Form1 aplicacaoPrincipal)
+        {
+            this.aplicacaoPrincipal = aplicacaoPrincipal;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conta novaConta = new ContaCorrente();
+            novaConta.Titular = new Cliente(titularConta.Text);
+            novaConta.Numero = Convert.ToInt32(numeroConta.Text);
+            this.aplicacaoPrincipal.adicionaConta(novaConta);
+        }
+    }
+}
---
+++ CaixaEletronico/CaixaEletronico/SeguroDeVida.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class SeguroDeVida: ITributavel
+    {
+        const double imposto = 42.0;
+
+        public double CalculaTributos()
+        {
+            return imposto;
+        }
+    }
+}
